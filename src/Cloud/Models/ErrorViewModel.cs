b25649390ff0b884300250137a850626aa5e6a3e
Asaf Rosentswaig <Asrosent@Microsoft.com>
2021-11-24 09:30:41 +0200

    Infra | Solution | Set Solution skeleton

---
+++ src/Cloud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntelliHome.Cloud
+{
+    public sealed class ErrorViewModel
+    {
+        public string? RequestId { get; init; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
