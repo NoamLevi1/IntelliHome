ae9055720fd7a42fb839a049d1c219e25dd66ad7
OfirGreenwald <75881026+OfirGreenwald@users.noreply.github.com>
2021-12-23 21:54:22 +0200

    Feature | CommunicationManager | Add ability to send Http request messages (#9)

---
+++ src/HomeAppliance.CommunicationManager/HttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace IntelliHome.HomeAppliance.CommunicationManager;
+
+public interface IHttpMessageHandler
+{
+    Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken);
+}
+
+public sealed class HttpMessageHandler : IHttpMessageHandler
+{
+    private readonly ILogger<HttpMessageHandler> _logger;
+    private readonly HttpClient _client;
+
+    public HttpMessageHandler(ILogger<HttpMessageHandler> logger)
+    {
+        _logger = logger;
+        _client = new HttpClient();
+    }
+
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"{nameof(SendAsync)} started");
+
+        var httpResponseMessage = await _client.SendAsync(httpRequestMessage, cancellationToken);
+
+        _logger.LogInformation($"{nameof(SendAsync)} finished");
+
+        return httpResponseMessage;
+    }
+}
