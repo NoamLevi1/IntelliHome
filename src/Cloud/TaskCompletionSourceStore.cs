0821287ca9e0948c9e8819872659073ef83ab008
idantraub@gmail.com <idantraub@gmail.com>
2022-02-09 21:05:03 +0200

    infra| cloud and server | working on TaskCompletionSourceStore and client method for sending response

---
+++ src/Cloud/TaskCompletionSourceStore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace IntelliHome.Cloud;
+
+public interface ITaskCompletionSourceStore
+{
+    void Add(Guid id, TaskCompletionSource<HttpResponseMessage> taskCompletionSource);
+    void SetResult(Guid id, HttpResponseMessage httpResponseMessage);
+    bool Remove(Guid id);
+}
+
+public class TaskCompletionSourceStore : ITaskCompletionSourceStore
+{
+    private readonly ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>> _dictionary;
+
+    public TaskCompletionSourceStore() => _dictionary=new ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>>();
+    
+
+    public void Add(Guid id, TaskCompletionSource<HttpResponseMessage> taskCompletionSource)
+    {
+        _dictionary.TryAdd(id, taskCompletionSource);
+    }
+
+    public void SetResult(Guid id, HttpResponseMessage httpResponseMessage)
+    {
+        if (!_dictionary.TryGetValue(id, out var taskCompletionSource))
+        {
+            throw new Exception($"Failed to get value of [{nameof(id)}={id}]");
+        };
+        taskCompletionSource.SetResult( httpResponseMessage);
+        _dictionary.TryUpdate(id, taskCompletionSource,taskCompletionSource);
+    }
+
+    public bool Remove(Guid id) => _dictionary.TryRemove(id,out var result);
+}
---
+++ src/HomeAppliance.CommunicationManager/SignalRClient.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace IntelliHome.HomeAppliance.CommunicationManager;
+public class SignalRClient : IHostedService
+{
+    private readonly ILogger<SignalRClient> _logger;
+    private readonly IHttpMessageHandler _httpMessageHandler;
+
+    private readonly HubConnection _connection;
+    private readonly CancellationTokenSource _cancellationTokenSource;
+
+    public SignalRClient(ILogger<SignalRClient> logger, IHttpMessageHandler httpMessageHandler)
+    {
+        _logger = logger;
+        _httpMessageHandler = httpMessageHandler;
+
+        _cancellationTokenSource = new CancellationTokenSource();
+        _connection = new HubConnectionBuilder().WithUrl("Https://localhost:7050/Connectionhub").WithAutomaticReconnect().Build();
+        ConfigureOnDisconnect();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _connection.On<HttpRequestMessage>(
+            nameof(ReceiveHttpMessageAsync),
+            ReceiveHttpMessageAsync);
+
+        await _connection.StartAsync(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cancellationTokenSource.Cancel();
+        await _connection.StopAsync(cancellationToken);
+    }
+
+    private void ConfigureOnDisconnect()
+    {
+        _connection.Closed += exception =>
+        {
+            _logger.LogInformation($"{nameof(ConfigureOnDisconnect)} Exception {exception}");
+            return Task.CompletedTask;
+        };
+    }
+
+    private async Task ReceiveHttpMessageAsync(HttpRequestMessage httpRequestMessage)
+    {
+        _logger.LogInformation($"{nameof(ReceiveHttpMessageAsync)} HTTP request received");
+        var response = await _httpMessageHandler.SendAsync(httpRequestMessage, _cancellationTokenSource.Token);
+        _logger.LogInformation($"{nameof(ReceiveHttpMessageAsync)} HTTP Response received wth [{nameof(response.StatusCode)}={response.StatusCode}]");
+
+        await _connection.InvokeAsync<HttpResponseMessage>("SendResponseBackToServer", response,_cancellationTokenSource.Token);
+        _logger.LogInformation($"{nameof(ReceiveHttpMessageAsync)} HTTP response sent back to server");
+    }
+}
