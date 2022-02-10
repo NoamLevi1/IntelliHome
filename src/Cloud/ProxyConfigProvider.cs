15bcab9acff19dcf0748ed13d67baf763d5c37f0
NoamLevi1 <noamlei@mta.ac.il>
2022-02-10 22:53:46 +0200

    Infra | ReverseProxy | add dynamic configuration for reverse proxy

---
+++ src/Cloud/ProxyConfigProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Primitives;
+using Yarp.ReverseProxy.Configuration;
+
+namespace IntelliHome.Cloud
+{
+    public sealed class CustomProxyConfigProvider : IProxyConfigProvider
+    {
+        private const string _defaultClusterId = "DefaultCluster";
+
+        public IProxyConfig GetConfig() =>
+            new CustomMemoryConfig
+            {
+                Routes = new[]
+                {
+                    new RouteConfig
+                    {
+                        RouteId = "DefaultRoute",
+                        ClusterId = _defaultClusterId,
+                        Match = new RouteMatch
+                        {
+                            Path = "{**catch-all}"
+                        }
+                    }
+                },
+                Clusters = new[]
+                {
+                    new ClusterConfig
+                    {
+                        ClusterId = _defaultClusterId,
+                        Destinations = new Dictionary<string, DestinationConfig>
+                        {
+                            ["DefaultDestination"] = new()
+                            {
+                                Address = "http://localhost:8123"
+                            }
+                        }
+                    }
+                }
+            };
+
+        private class CustomMemoryConfig : IProxyConfig
+        {
+            private static readonly CancellationTokenSource _cancellationTokenSource = new();
+
+            public IReadOnlyList<RouteConfig> Routes { get; init; } = Array.Empty<RouteConfig>();
+
+            public IReadOnlyList<ClusterConfig> Clusters { get; init; } = Array.Empty<ClusterConfig>();
+
+            public IChangeToken ChangeToken { get; } = new CancellationChangeToken(_cancellationTokenSource.Token);
+        }
+    }
+}
