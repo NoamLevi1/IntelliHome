ffa65e38023393eb2a767c55c02b6e68915ac585
OfirGreenwald <75881026+OfirGreenwald@users.noreply.github.com>
2022-02-19 18:58:57 +0200

    Infra | Configuration | Add ConfigurationManager (#19)

---
+++ src/Common/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IntelliHome.Common;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddConfigurationManager<TConfiguration>(this IServiceCollection services)
+        where TConfiguration : class, IServiceConfiguration =>
+        services.
+            AddSingleton<IServiceConfiguration, TConfiguration>().
+            AddSingleton<IConfigurationManager, ConfigurationManager>();
+}
