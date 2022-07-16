bc962c1fc07b08b87d7c8aa5e9d7a1928d7972f8
Asafrose <asafr1993@gmail.com>
2022-07-16 13:27:33 +0300

    Infra | Models | Add HomeAppliance Model (#27)
    * Infra | Models | Add HomeAppliance Model
    
    * revision 2
    
    Co-authored-by: asrosent <asrosent@microsoft.com>

---
+++ src/Cloud/Models/HomeAppliance.cs
@@ -0,0 +1,11 @@
+namespace IntelliHome.Cloud;
+
+public sealed class HomeAppliance
+{
+    public Guid Id { get; }
+    public string? Name { get; set; }
+    public bool IsConnected { get; set; }
+
+    public HomeAppliance(Guid id) => 
+        Id = id;
+}
