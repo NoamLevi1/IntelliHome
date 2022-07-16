d4877f681d190fd177eef17a9299a7011b69bab4
idantraub <59113754+idantraub@users.noreply.github.com>
2022-07-16 16:24:22 +0300

    Feature | Front | Introduce HomeAppliance Catalog (#29)

---
+++ src/Cloud/Extensions/EndpointRouteBuilderExtension.cs
@@ -0,0 +1,51 @@
+﻿using IntelliHome.Common;
+
+namespace IntelliHome.Cloud.Extensions;
+
+public static class EndpointRouteBuilderExtension
+{
+    public static void MapControllerRouteWithHostNamePrefix(
+        this IEndpointRouteBuilder builder,
+        string name,
+        string pattern,
+        string prefix)
+    {
+        Ensure.NotNull(builder);
+        Ensure.NotNullOrWhiteSpace(name);
+        Ensure.NotNullOrWhiteSpace(pattern);
+        Ensure.NotNullOrWhiteSpace(prefix);
+
+        builder.MapControllerRoute(
+            name,
+            pattern,
+            constraints: new
+            {
+                customConstraint = new HostNamePrefixRouteConstraint(prefix)
+            });
+    }
+
+    private class HostNamePrefixRouteConstraint : IRouteConstraint
+    {
+        private readonly string _prefix;
+
+        public HostNamePrefixRouteConstraint(string prefix)
+        {
+            Ensure.NotNullOrWhiteSpace(prefix);
+
+            _prefix = prefix;
+        }
+
+        public bool Match(
+            HttpContext? httpContext,
+            IRouter? route,
+            string routeKey,
+            RouteValueDictionary values,
+            RouteDirection routeDirection) =>
+            httpContext?.
+                Request.
+                Host.
+                Host.
+                StartsWith(_prefix, StringComparison.OrdinalIgnoreCase) ??
+            false;
+    }
+}
