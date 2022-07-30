0c7b567cd38a5805f78a40751f28e74368125c0b
asrosent <asrosent@microsoft.com>
2022-07-30 16:06:04 +0300

    Infra | Database | Add support for database

--- src/Cloud/Controllers/HomeApplianceCatalogController.cs
+++ src/Cloud/Controllers/HomeApplianceCatalogController.cs
@@ -1,44 +1,42 @@
 ﻿using IntelliHome.Common;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 namespace IntelliHome.Cloud.Controllers
 {
     public sealed class HomeApplianceCatalogController : Controller
     {
-        static readonly IReadOnlyCollection<HomeAppliance> _devicesList =
-            new[]
-            {
-                new HomeAppliance(Guid.NewGuid()) {Name = "Living Room", IsConnected = true},
-                new HomeAppliance(Guid.NewGuid()) {Name = "Dining Room", IsConnected = false},
-                new HomeAppliance(Guid.NewGuid()) {Name = "Sport Room", IsConnected = false},
-                new HomeAppliance(Guid.NewGuid()) {Name = "Beds Room", IsConnected = true},
-                new HomeAppliance(Guid.NewGuid()) {Name = "Children Room", IsConnected = true},
-                new HomeAppliance(Guid.NewGuid()) {Name = "Dog's Room", IsConnected = true}
-            };
+        private readonly IDatabase _database;
         private readonly WebApplicationConfiguration _webApplicationConfiguration;
 
-        public HomeApplianceCatalogController(IConfigurationManager configurationManager) =>
+        public HomeApplianceCatalogController(IConfigurationManager configurationManager, IDatabase database)
+        {
+            _database = database;
             _webApplicationConfiguration = configurationManager.Get<WebApplicationConfiguration>();
+        }
 
-        public IActionResult Index() =>
+        public async Task<IActionResult> Index() =>
             View(
-                _devicesList.
-                    OrderByDescending(_ => _.IsConnected).
-                    Select(
-                        (device, number) =>
+                (await (await _database.
+                        HomeAppliances.
+                        FindAsync(_ => true)).
+                    ToListAsync()).
+                OrderByDescending(_ => _.IsConnected).
+                Select(
+                    (device, number) =>
+                    {
+                        var uri = new UriBuilder
                         {
-                            var uri = new UriBuilder
-                            {
-                                Scheme = _webApplicationConfiguration.ServerUrl.Scheme,
-                                Port = _webApplicationConfiguration.ServerUrl.Port,
-                                Host = $"{device.Id}.{_webApplicationConfiguration.ServerUrl.Host}"
-                            }.Uri;
+                            Scheme = _webApplicationConfiguration.ServerUrl.Scheme,
+                            Port = _webApplicationConfiguration.ServerUrl.Port,
+                            Host = $"{device.Id}.{_webApplicationConfiguration.ServerUrl.Host}"
+                        }.Uri;
 
-                            return new HomeApplianceCatalogModel(
-                                number + 1,
-                                device.Name ?? "Unconfigured",
-                                device.IsConnected,
-                                uri);
-                        }));
+                        return new HomeApplianceCatalogModel(
+                            number + 1,
+                            device.Name ?? "Unconfigured",
+                            device.IsConnected,
+                            uri);
+                    }));
     }
 }
---
+++ src/Common/Data/HomeAppliance.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace IntelliHome.Common;
+
+public sealed class HomeAppliance
+{
+    [BsonId]
+    public Guid Id { get; }
+
+    public string? Name { get; set; }
+    public Guid OwnerId => Guid.Empty;
+    public string? ConnectionId { get; set; }
+
+    public bool IsConnected => ConnectionId != null;
+
+    [BsonConstructor]
+    public HomeAppliance(Guid id) =>
+        Id = id;
+}
