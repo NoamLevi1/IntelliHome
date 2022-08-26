4cfa266f1466682f5415603b85ee1dc1d9cea621
NoamLevi1 <88629793+NoamLevi1@users.noreply.github.com>
2022-08-26 16:47:52 +0300

    Feature | HomeApplianceCatalog | Add ability to add new home appliance (#36)

--- src/Cloud/Models/EditHomeApplianceModel.cs
+++ src/Cloud/Models/EditHomeApplianceModel.cs
@@ -1,7 +1,10 @@
-﻿namespace IntelliHome.Cloud;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntelliHome.Cloud;
 
 public sealed class EditHomeApplianceModel
 {
     public Guid Id { get; set; }
+    [Required]
     public string? Name { get; set; }
 }
