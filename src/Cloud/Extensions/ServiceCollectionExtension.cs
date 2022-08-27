1c122d39873c4dea37cb1ff6c29a315ceb16e27f
asrosent <asrosent@microsoft.com>
2022-08-27 19:06:54 +0300

    Fix | Login | Add proper redirection to login

--- src/Cloud/Extensions/ServiceCollectionExtension.cs
+++ src/Cloud/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
 using IntelliHome.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 
 namespace IntelliHome.Cloud.Extensions;
@@ -26,9 +28,12 @@ public static class ServiceCollectionExtension
         Ensure.NotNull(services);
         Ensure.NotNullOrWhiteSpace(host);
 
-        return services.ConfigureApplicationCookie(
+        return services.
+            AddScoped(_ => new ExtendedCookieAuthenticationEvents(host)).
+            ConfigureApplicationCookie(
             options =>
             {
+                options.EventsType = typeof(ExtendedCookieAuthenticationEvents);
                 options.Cookie.Domain = $".{host}";
                 options.Cookie.SameSite = SameSiteMode.Lax;
             });
@@ -43,5 +48,44 @@ public static class ServiceCollectionExtension
 
         services.AddOptions<AuthorizationOptions>().Configure(configure);
         return services.AddAuthorization();
+    }
+
+    private class ExtendedCookieAuthenticationEvents : CookieAuthenticationEvents
+    {
+        private readonly string _host;
+
+        public ExtendedCookieAuthenticationEvents(string host) => 
+            _host = host;
+        
+        public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            var redirectUri = new Uri(context.RedirectUri);
+
+            var redirectPathAndQuery = Uri.UnescapeDataString(redirectUri.Query.Replace("?ReturnUrl=", string.Empty)).Split("?");
+            var returnUrlBuilder = new UriBuilder
+            {
+                Host = redirectUri.Host,
+                Scheme = redirectUri.Scheme,
+                Path = redirectPathAndQuery[0],
+                Port = redirectUri.Port
+            };
+            if (redirectPathAndQuery.Length > 1)
+            {
+                returnUrlBuilder.Query = redirectPathAndQuery[1];
+            }
+            
+            var returnUrl = Uri.EscapeDataString(returnUrlBuilder.ToString());
+            var uri = new UriBuilder
+            {
+                Scheme = context.Request.Scheme,
+                Host = $"portal.{_host}",
+                Port = context.Request.Host.Port ?? -1,
+                Path = redirectUri.AbsolutePath,
+                Query = $"ReturnUrl={returnUrl}"
+            }.Uri;
+            
+            context.Response.Redirect(uri.ToString());
+            return Task.CompletedTask;
+        }
     }
 }
